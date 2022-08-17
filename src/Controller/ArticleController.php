84c53e6fe035b193ff8a38f299ff9beeedaf063f
Anas Saoui <62400765+Anas-debug@users.noreply.github.com>
2022-08-18 00:39:50 +0100

    Restrained HTTP verbs

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -8,6 +8,7 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 class ArticleController{
     /**
      * @Route("/")
+     * @Method({"GET", "POST"})
      */
     public function index() {
         return new Response("<h1 style='color:purple'>Article Post<h1>");
