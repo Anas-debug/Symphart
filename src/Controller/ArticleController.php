9e741f877acb558bc38999f7f40b9ca871590223
Anas Saoui <62400765+Anas-debug@users.noreply.github.com>
2022-08-17 23:59:51 +0100

    Demo article route

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -2,3 +2,9 @@
 namespace App\Controller;
 
 use Symfony\Component\HttpFoundation\Response;
+
+class ArticleController{
+    public function index() {
+        return new Response("<h1 style='color:purple'>Article Post<h1>");
+    }
+}
