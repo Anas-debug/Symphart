ae0551d6c55defd09fae4c4d3364ba0458989f33
Anas Saoui <62400765+Anas-debug@users.noreply.github.com>
2022-08-18 00:53:29 +0100

    Using Abstract Controller in order to use Twig.

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -4,8 +4,10 @@ namespace App\Controller;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
-class ArticleController{
+
+class ArticleController extends AbstractController{
     /**
      * @Route("/")
      * @Method({"GET", "POST"})
