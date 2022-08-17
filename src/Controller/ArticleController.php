6336e7a1fe6ea6e4a484874bbb9180b2e563d72a
Anas Saoui <62400765+Anas-debug@users.noreply.github.com>
2022-08-18 00:34:34 +0100

    We used annotations instead of routes

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -2,8 +2,13 @@
 namespace App\Controller;
 
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 
 class ArticleController{
+    /**
+     * @Route("/")
+     */
     public function index() {
         return new Response("<h1 style='color:purple'>Article Post<h1>");
     }
